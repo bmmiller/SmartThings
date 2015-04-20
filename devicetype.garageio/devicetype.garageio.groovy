5dfbf9cace890e531f7ad8fe389ce27530ae1f34
Brandon Miller <bmmiller@gmail.com>
2015-04-20 17:09:01 -0500

    Update for Garageio

--- devicetype.garageio/devicetype.garageio.groovy
+++ devicetype.garageio/devicetype.garageio.groovy
@@ -138,18 +138,16 @@ def push() {
     // The above auth check isn't perfect.  This is to account for a valid response that 
     // states that the auth token has been rejected.  
     def tryPost = true
-    def responseMessage
+    def responseCode
     
     while (tryPost) {
         httpPost(params) { response->
             log.debug response.data
-            responseMessage = response.data.message
+            responseCode = response.data.code
         }
         
-        // Check here is fragile.  If they change their message, it will break. They have 
-        // a success flag, but I don't want to keep retrying the post if the failure isn't
-        // auth related.  They have a code, but I don't know what their codes necessarily mean.
-        if (responseMessage == "Authentication Token Expired. Authentication Required.") {
+        if (responseCode == "401") {
+        	log.debug "Retrying login..."
             login()           
             sleep(2) // Wait for 2 seconds to allow Garageio servers recognize the new auth token they just created
         } else {
